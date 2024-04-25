8e246197c5fc7f274398d06e1c19b518a03cef63
John Velasco <C=John.Velasco@charter.com>
2024-04-25 16:10:53 -0400

    Updating

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,14 +1,9 @@
 import { fetchPlaceholders } from '../../scripts/aem.js';
 
-
 function iniFrame() {
   if (window.location !== window.parent.location) {
       // The page is in an iFrames
-     console.log("The page is in an iFrame");
-  }
-  else {
-      // The page is not in an iFrame
-      console.log("The page is not in an iFrame");
+     return true;
   }
 }
 
