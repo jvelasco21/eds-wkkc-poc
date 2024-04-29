2e9510e53ac223f01f8319d3a3fbd42e5f00be3d
John Velasco <C=John.Velasco@charter.com>
2024-04-29 13:57:05 -0400

    Defining WKKC Buttons

--- scripts/aem.js
+++ scripts/aem.js
@@ -389,7 +389,7 @@ function decorateButtons(element) {
       const twoup = a.parentElement.parentElement;
       if (!a.querySelector('img')) {
         if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-          a.className = 'button'; // default
+          a.className = 'button default'; // default
           up.classList.add('button-container');
         }
         if (
