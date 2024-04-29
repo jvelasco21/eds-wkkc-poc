cff615c6a87e94745fccf8ea002e5c94eb03c3d7
John Velasco <C=John.Velasco@charter.com>
2024-04-29 16:53:31 -0400

    Hero JS adjustment

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -6,11 +6,16 @@ export default function decorate(block) {
     foreground.className = 'hero-foreground';
 
     [...block.children].forEach((item, i) => {
-      if (i !== 0) {
+      const childDiv = item.querySelector('div');
+      if (i !== 1) {
         item.classList.add(`hero-foreground-item-${i}`);
         foreground.append(item);
       } else {
         item.className = 'hero-background';
+      }
+
+      if (childDiv.innerHTML === '') {
+        childDiv.parentElement.remove();
       }
     });
 
