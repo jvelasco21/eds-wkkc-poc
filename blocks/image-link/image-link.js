e12ec2035690e5c75bfba2c2d775193871d8928b
John Velasco <C=John.Velasco@charter.com>
2024-05-10 13:18:42 -0400

    Adding Image Link Block

--- blocks/image-link/image-link.js
+++ blocks/image-link/image-link.js
@@ -1,3 +1,11 @@
 export default async function decorate(block) {
-  block.classList.add('test-yo');
+  const linkTag = document.createElement('a');
+  const firstPara = block.querySelector('p');
+  const linkHref = block.querySelector('.button').getAttribute('href');
+
+  linkTag.setAttribute('href', linkHref);
+  linkTag.innerHTML = firstPara.innerHTML;
+  firstPara.parentNode.replaceChild(linkTag, firstPara);
+
+  block.lastElementChild.remove();
 }
