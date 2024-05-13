c5be67df299503c2410808f66687dbfec40fc021
John Velasco <C=John.Velasco@charter.com>
2024-05-13 10:11:45 -0400

    Updating model and logic

--- blocks/image-block/image-block.js
+++ blocks/image-block/image-block.js
@@ -3,9 +3,11 @@ export default async function decorate(block) {
   const firstPara = block.querySelector('p');
   const linkHref = block.querySelector('.button').getAttribute('href');
 
-  linkTag.setAttribute('href', linkHref);
-  linkTag.innerHTML = firstPara.innerHTML;
-  firstPara.parentNode.replaceChild(linkTag, firstPara);
+  if (linkHref) {
+    linkTag.setAttribute('href', linkHref);
+    linkTag.innerHTML = firstPara.innerHTML;
+    firstPara.parentNode.replaceChild(linkTag, firstPara);
 
-  block.lastElementChild.remove();
+    block.lastElementChild.remove();
+  }
 }
